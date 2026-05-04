39688ebdcbcb97dff07b2c46ffc691b92acb998d
JDPM <josedanielpmj@gmail.com>
2026-05-04 01:24:15 -0400

    Configuración para el despliegue rápido

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,4 @@ cd /home/apps/renew-group-system || exit
 systemctl stop renew-group-system.service
 git pull
 npm install
-npm run build
 systemctl start renew-group-system.service
--- start_app.sh
+++ start_app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export NVM_DIR="$HOME/.nvm"
 source "$NVM_DIR/nvm.sh"
-cd /home/apps/renew-group-system && npm start
+cd /home/apps/renew-group-system && npm run dev:full
