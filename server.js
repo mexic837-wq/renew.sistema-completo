381242be8e44ff97e06306777fbe2cf50215904f
Miguel <mexic837@gmail.com>
2026-05-06 22:03:01 -0400

    Fix: Increased global express body limits to 500MB to prevent 413 errors on large uploads

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ process.on('uncaughtException', (err) => {
 });
 
 app.use(cors());
-app.use(express.json({ limit: '100mb' })); 
-app.use(express.urlencoded({ limit: '100mb', extended: true }));
+app.use(express.json({ limit: '500mb' })); 
+app.use(express.urlencoded({ limit: '500mb', extended: true }));
 app.use(express.static(path.join(__dirname)));
 
 // Static files are served above via express.static(__dirname)
