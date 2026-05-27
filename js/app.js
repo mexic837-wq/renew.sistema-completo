0ec5e99892ba8918fad7ea4a2a4dd2b64f86a664
Miguel <mexic837@gmail.com>
2026-05-26 23:32:11 -0400

    Bump cache busters to force browser to load new calendar.js

--- js/app.js
+++ js/app.js
@@ -34,7 +34,7 @@ import { renderMenu }       from './screens/menu.js?v=2';
 import { renderInventoryTech } from './screens/inventory.js';
 import { renderClients } from './screens/clients.js';
 import { renderCallCenter } from './screens/callCenter.js';
-import { renderMiCalendario } from './screens/calendar.js?v=2';
+import { renderMiCalendario } from './screens/calendar.js?v=3';
 import { renderMiMapa } from './screens/mapa.js';
 import { renderMiEquipo } from './screens/equipo.js';
 import { renderPartners } from './screens/partners.js';
