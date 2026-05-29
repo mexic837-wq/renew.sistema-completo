a81aeba52abe03bad370d9cd9bb7c1a5447857c2
Miguel <mexic837@gmail.com>
2026-05-29 17:27:12 -0400

    Fix project chat save error by stripping creador_id from proyectos_dinamicos

--- js/api.js
+++ js/api.js
@@ -331,7 +331,7 @@ export async function saveGranular(table, records) {
       ultima_actividad_label, rol_fase, is_locked,
       direccion, nombre_cliente, telefono_cliente, email_cliente,
       email, telefono, etapa, fase_orden, total_fases,
-      zip, licencia, id_photo, 
+      zip, licencia, id_photo, creador_id,
       contrato_url, contrato_solar_url, contrato_water_url, contrato_home_url,
       ...rest 
     }) => ({
